4a1e58fd8331c6fd9010614ea1758320013d62ed
jose Jerez <josejerez2296@gmail.com>
2017-11-22 22:52:43 +0100

    Perfil,sesiones
    funcionalidad de menu (Perfil) , sesiones se un usuario y cerrar sesion

--- app.js
+++ app.js
@@ -10,10 +10,21 @@ var users = require('./routes/users');
 
 var firebase = require("firebase");
 
-var usuario;
+
+var Session = require('express-session');
 
 
 var app = express();
+
+
+app.use(Session({
+    secret: 'asdqwe123456',
+    saveUnInitialized: true,
+    resave: true
+}));
+
+
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -23,7 +34,7 @@ app.set('view engine', 'pug');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -31,24 +42,22 @@ app.use('/', index);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Pagina no encontrada');
-  err.status = 404;
-  next(err);
+app.use(function (req, res, next) {
+    var err = new Error('Pagina no encontrada');
+    err.status = 404;
+    next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
-
-
 
 
 // Initialize Firebase
@@ -61,7 +70,6 @@ var config = {
     messagingSenderId: "225171417559"
 };
 firebase.initializeApp(config);
-
 
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 var firebase = require('firebase');
 
-
+var datos;
 /* GET home page. */
 router.get('/', function (req, res, next) {
 
@@ -18,16 +18,43 @@ router.get('/Registro', function (req, res, next) {
 
 
 router.get('/Login', function (req, res, next) {
-    res.render('Login', {title: 'Login'});
+    console.log(req.session.userName);
+
+    if (req.session.userName) {
+
+        usuario = firebase.auth().currentUser;
+        var usuarioUid = usuario.uid;
+
+        firebase.database().ref("usuarios/").on("value", function (dataSnapshot) {
+
+            var datosUsuario = dataSnapshot.val()[usuarioUid];
+            console.log(datosUsuario, usuarioUid);
+
+            res.render('Perfil', {
+                    datos: datosUsuario
+                }
+            );
+
+        })
+    } else {
+        res.render('Login', {title: 'Login'});
+    }
 });
 
 router.get('/Perfil', function (req, res, next) {
-    res.render('Perfil', {title: 'Login'});
+    console.log("PERFIL"+datos);
+    res.render('Perfil', {datos: datos});
 });
 
 // router.get('/DatosUsuario', function (req, res, next) {
 //     res.render('DatosUsuario', {title: 'DatosUsuario'});
 // });
+
+
+router.get("/cerrar",function (req,res,next) {
+    req.session.destroy();
+    res.render("Login");
+});
 
 
 router.post('/registroComp', function (req, res, next) {
@@ -64,36 +91,34 @@ router.post('/registroComp', function (req, res, next) {
 });
 
 
-router.post('/login', function (req, res, next) {
+router.post('/Login', function (req, res, next) {
     //console.log(req.body);
-
     firebase.auth().signInWithEmailAndPassword(req.body.email, req.body.contraseña)
         .then(function (t) {
             //console.log(t);
             usuario = firebase.auth().currentUser;
-            var usuarioUid = usuario.uid
+            var usuarioUid = usuario.uid;
             if (usuario.emailVerified) {
 
                 firebase.database().ref("usuarios/").on("value", function (dataSnapshot) {
 
                     var datosUsuario = dataSnapshot.val()[usuarioUid];
+                    console.log(datosUsuario, usuarioUid);
 
-                    console.log(datosUsuario,usuarioUid);
+                    console.log(datosUsuario.nombre);
 
                     if (datosUsuario === undefined) {
                         res.render('DatosUsuario');
-                    }else{
+                    } else {
+                        req.session.userName = datosUsuario.nombre;
+                        datos = datosUsuario;//Para poder enviar los datos al get de /Perfil
+
                         res.render('Perfil', {
                                 datos: datosUsuario
                             }
                         );
                     }
-
-
-
                 })
-
-
             } else {
                 console.log("No verif");
                 res.render('Login', {title: 'Login', warning: "Correo no verificado"});
@@ -103,20 +128,18 @@ router.post('/login', function (req, res, next) {
             console.log(error);
             res.render('Login', {title: 'Login', error: "Usuario no encontrado"});
         });
-
-
 });
 
 router.post('/datosUsuario', function (req, res, next) {
     console.log(req.body);
 
 
-    var usuarios = firebase.database().ref("usuarios/"+usuario.uid);
+    var usuarios = firebase.database().ref("usuarios/" + usuario.uid);
     usuarios.set({
-    	"nombre": req.body.nombre,
-    	"descripcion": req.body.descripcion,
-        "canciones": {"cancion1": "", "cancion2":""},
-        "playlists": {"playlist1": "", "playlist2":""}
+        "nombre": req.body.nombre,
+        "descripcion": req.body.descripcion,
+        "canciones": {},
+        "playlists": {}
     });
 
 
