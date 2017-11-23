252539851b875cf155734d0ab9baf43dd92a89e5
jose Jerez <josejerez2296@gmail.com>
2017-11-23 23:14:01 +0100

    Agregar Canciones a la base de datos y visualzacion

--- routes/index.js
+++ routes/index.js
@@ -4,187 +4,251 @@ var router = express.Router();
 var firebase = require('firebase');
 
 var datos;
+var usuario;
 /* GET home page. */
 router.get('/', function (req, res, next) {
-
-	res.render('Inicio')
-
+    res.render('Inicio');
 });
 
 
 router.get('/Registro', function (req, res, next) {
-	res.render('Registro', {title: 'Registro'});
+    res.render('Registro', {title: 'Registro'});
 });
 
 
 router.get('/Login', function (req, res, next) {
-	console.log(req.session.userName);
+    console.log(req.session.userName);
 
-	if (req.session.userName) {
+    if (req.session.userName) {
 
-		usuario = firebase.auth().currentUser;
-		var usuarioUid = usuario.uid;
+        usuario = firebase.auth().currentUser;
+        var usuarioUid = usuario.uid;
 
-		firebase.database().ref("usuarios/").on("value", function (dataSnapshot) {
+        firebase.database().ref("usuarios/").once("value", function (dataSnapshot) {
 
-			var datosUsuario = dataSnapshot.val()[usuarioUid];
-			console.log(datosUsuario, usuarioUid);
+            var datosUsuario = dataSnapshot.val()[usuarioUid];
+            console.log(datosUsuario, usuarioUid);
 
-			res.render('Perfil', {
-					datos: datosUsuario
-				}
-			);
+            res.render('Perfil', {
+                    datos: datosUsuario
+                }
+            );
 
-		})
-	} else {
-		res.render('Login', {title: 'Login'});
-	}
+        })
+    } else {
+        res.render('Login', {title: 'Login'});
+    }
 });
 
 router.get('/Perfil', function (req, res, next) {
-	console.log("PERFIL" + datos);
-	res.render('Perfil', {datos: datos});
-});
+    if (req.session.userName) {
+        console.log("PERFIL" + datos.nombre);
+        res.render('Perfil', {datos: datos});
+    } else {
+        res.render("Login")
+    }
 
-// router.get('/DatosUsuario', function (req, res, next) {
-//     res.render('DatosUsuario', {title: 'DatosUsuario'});
-// });
+});
 
 
 router.get("/cerrar", function (req, res, next) {
-	req.session.destroy();
-	res.render("Login");
+    req.session.destroy();
+    res.render("Login");
 });
 
 
 router.get("/eliminarUsuario", function (req, res, next) {
 
-	usuario = firebase.auth().currentUser;
+    usuario = firebase.auth().currentUser;
 
-	firebase.database().ref("usuarios/" + usuario.uid).remove()
-		.then(function (t) {
-			console.log(t);
-			usuario.delete()
-				.then(function (t) {
-					console.log("Usuario Elimiando", t);
-					req.session.destroy();
-					res.render("Registro");
-				})
-		});
+    firebase.database().ref("usuarios/" + usuario.uid).remove()
+        .then(function (t) {
+            console.log(t);
+            usuario.delete()
+                .then(function (t) {
+                    console.log("Usuario Elimiando", t);
+                    req.session.destroy();
+                    res.render("Registro");
+                })
+        });
 
 
 })
 
 
 router.post('/registroComp', function (req, res, next) {
-	//console.log(req.body);
+    //console.log(req.body);
 
-	firebase.auth().createUserWithEmailAndPassword(req.body.email, req.body.contraseña)
-		.then(function (t) {
-			usuario = firebase.auth().currentUser;
-
-
-			usuario.sendEmailVerification()
-				.then(function () {
-					console.log(req.body);
-					res.render('Login', {title: 'Login'});
-					// Email sent.
-				}).catch(function (error) {
-				// An error happened.
-			});
-
-		})
-		.catch(function (error) {
-			console.log(error);
-			res.render('Registro', {title: 'Registro'});
-		});
+    firebase.auth().createUserWithEmailAndPassword(req.body.email, req.body.contraseña)
+        .then(function (t) {
+            usuario = firebase.auth().currentUser;
 
 
-	// var usuarios = firebase.database().ref("usuarios/");
-	// usuarios.push({
-	// 	"nombre": req.body.nombre,
-	// 	"contraseña": req.body.contraseña
-	// });
+            usuario.sendEmailVerification()
+                .then(function () {
+                    console.log(req.body);
+                    res.render('Login', {title: 'Login'});
+                    // Email sent.
+                }).catch(function (error) {
+                // An error happened.
+            });
+
+        })
+        .catch(function (error) {
+            console.log(error);
+            res.render('Registro', {title: 'Registro'});
+        });
+
+
+    // var usuarios = firebase.database().ref("usuarios/");
+    // usuarios.push({
+    // 	"nombre": req.body.nombre,
+    // 	"contraseña": req.body.contraseña
+    // });
 
 
 });
 
 
 router.post('/Login', function (req, res, next) {
-	//console.log(req.body);
-	firebase.auth().signInWithEmailAndPassword(req.body.email, req.body.contraseña)
-		.then(function (t) {
-			//console.log(t);
-			usuario = firebase.auth().currentUser;
-			var usuarioUid = usuario.uid;
-			if (usuario.emailVerified) {
+    //console.log(req.body);
+    firebase.auth().signInWithEmailAndPassword(req.body.email, req.body.contraseña)
+        .then(function (t) {
+            //console.log(t);
+            usuario = firebase.auth().currentUser;
+            var usuarioUid = usuario.uid;
+            if (usuario.emailVerified) {
 
-				firebase.database().ref("usuarios/").on("value", function (dataSnapshot) {
+                firebase.database().ref("usuarios/").once("value", function (dataSnapshot) {
 
-					var datosUsuario = dataSnapshot.val()[usuarioUid];
-					/*console.log(datosUsuario, usuarioUid);
+                    var datosUsuario = dataSnapshot.val()[usuarioUid];
+                    /*console.log(datosUsuario, usuarioUid);
 
-					console.log(datosUsuario.nombre);*/
+                    console.log(datosUsuario.nombre);*/
 
-					if (!datosUsuario) {
-						res.render('DatosUsuario');
-					} else {
-						req.session.userName = datosUsuario.nombre;
-						datos = datosUsuario;//Para poder enviar los datos al get de /Perfil
+                    if (!datosUsuario) {
+                        res.render('DatosUsuario');
+                    } else {
+                        req.session.userName = datosUsuario.nombre;
+                        datos = datosUsuario;//Para poder enviar los datos al get de /Perfil
 
-						res.render('Perfil', {
-								datos: datosUsuario
-							}
-						);
-					}
-				})
-			} else {
-				console.log("No verif");
-				res.render('Login', {title: 'Login', warning: "Correo no verificado"});
-			}
-		})
-		.catch(function (error) {
-			console.log(error);
-			res.render('Login', {title: 'Login', error: "Usuario no encontrado"});
-		});
+                        res.render('Perfil', {
+                                datos: datosUsuario
+                            }
+                        );
+                    }
+                })
+            } else {
+                console.log("No verif");
+                res.render('Login', {title: 'Login', warning: "Correo no verificado"});
+            }
+        })
+        .catch(function (error) {
+            console.log(error);
+            res.render('Login', {title: 'Login', error: "Usuario no encontrado"});
+        });
 });
 
 router.post('/datosUsuario', function (req, res, next) {
-	console.log(req.body);
+    console.log(req.body);
 
 
-	var usuarios = firebase.database().ref("usuarios/" + usuario.uid);
-	usuarios.set({
-		"nombre": req.body.nombre,
-		"descripcion": req.body.descripcion,
-		"canciones": {},
-		"playlists": {}
-	});
+    var usuarios = firebase.database().ref("usuarios/" + usuario.uid);
+    usuarios.set({
+        "nombre": req.body.nombre,
+        "descripcion": req.body.descripcion,
+        "canciones": 0,
+        "playlists": 0
+    });
 
 
-	usuario = firebase.auth().currentUser;
-	var usuarioUid = usuario.uid;
+    usuario = firebase.auth().currentUser;
+    var usuarioUid = usuario.uid;
 
-	firebase.database().ref("usuarios/").on("value", function (dataSnapshot) {
+    firebase.database().ref("usuarios/").once("value", function (dataSnapshot) {
 
-		var datosUsuario = dataSnapshot.val()[usuarioUid];
-		/*console.log(datosUsuario, usuarioUid);
+        var datosUsuario = dataSnapshot.val()[usuarioUid];
+        /*console.log(datosUsuario, usuarioUid);
 
-		console.log(datosUsuario.nombre);*/
+        console.log(datosUsuario.nombre);*/
 
 
-		req.session.userName = datosUsuario.nombre;
-		datos = datosUsuario;//Para poder enviar los datos al get de /Perfil
+        req.session.userName = datosUsuario.nombre;
+        datos = datosUsuario;//Para poder enviar los datos al get de /Perfil
 
-		res.render('Perfil', {
-				datos: datosUsuario
-			}
-		);
+        res.render('Perfil', {
+                datos: datosUsuario
+            }
+        );
 
-	})
+    })
 
 
 });
+
+
+//Acceso a la vista TopCanciones con los datos del usuario de firebase, encaso de no haber iniciado session se va al login
+router.get("/TopCanciones", function (req, res, next) {
+
+    if (req.session.userName) {
+        usuario = firebase.auth().currentUser;
+        var usuarioUid = usuario.uid;
+
+        firebase.database().ref("usuarios/").once("value", function (dataSnapshot) {
+
+            var datosUsuario = dataSnapshot.val()[usuarioUid];
+            /*console.log(datosUsuario, usuarioUid);
+
+            console.log(datosUsuario.nombre);*/
+
+
+            req.session.userName = datosUsuario.nombre;
+            datos = datosUsuario;//Para poder enviar los datos al get de /Perfil
+
+            res.render("TopCanciones", {pagina: "Top Canciones", datos: datos})
+
+
+        })
+    } else {
+        res.render("Login")
+    }
+
+})
+
+
+//Post de la nueva cancion (TOPCANCIONES) del usuario a la base de datos
+router.post("/TopCanciones", function (req, res, next) {
+
+    //console.log(req.body);
+
+    usuario = firebase.auth().currentUser;
+    var usuarioUid = usuario.uid;
+
+
+    var canciones = [];
+
+
+    len = datos.canciones.length;
+    for (i = 0; i < len; i += 1) {
+        canciones.push(datos.canciones[i])
+
+    }
+
+    canciones.push(req.body.cancion);
+
+    firebase.database().ref("usuarios/" + usuarioUid + "/canciones")
+        .push(
+            canciones
+        )
+
+    firebase.database().ref("usuarios/").once("value", function (dataSnapshot) {
+        var datosUsuario = dataSnapshot.val()[usuarioUid];
+        req.session.userName = datosUsuario.nombre;
+        datos = datosUsuario;//Para poder enviar los datos al get de /Perfil
+
+        res.render("TopCanciones", {pagina: "Top Canciones", datos: datos})
+    })
+})
 
 
 module.exports = router;
